269c89f35836d9b050abde7a1b25414c966ce9b0
ziyic <ziyicdev@gmail.com>
2019-11-20 12:21:02 +0000

    11.20 submit

--- ziyic AaVR project/Assets/PedestrianRoute.cs
+++ ziyic AaVR project/Assets/PedestrianRoute.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class PedestrianRoute : MonoBehaviour
 {
     public List<Transform> wps;
+    public List<Transform> route;
+    public float targetWP;
+    public int routeNumber = 0;
     // Start is called before the first frame update
     void Start()
     {
@@ -41,4 +44,137 @@ public class PedestrianRoute : MonoBehaviour
     {
         
     }
-}
+
+    void SetRoute()
+    {//randomise the next route
+        routeNumber = Random.Range(0, 12);
+        //set the routewaypoints
+        if (routeNumber == 0)
+        {
+            route = new List<Transform>
+            {
+                wps[0],
+                wps[4],
+                wps[5],
+                wps[6]
+            };
+        }
+        else if (routeNumber == 1)
+        {
+            route = new List<Transform>
+            {
+                wps[0],
+                wps[4],
+                wps[5],
+                wps[7]
+            };
+        }
+        else if (routeNumber == 2)
+        {
+            route = new List<Transform>
+            {
+                wps[2],
+                wps[1],
+                wps[4],
+                wps[5],
+                wps[6]
+            };
+        }
+        else if (routeNumber == 3)
+        {
+            route = new List<Transform>
+            {
+                wps[2],
+                wps[1],
+                wps[4],
+                wps[5],
+                wps[7]
+            };
+        }
+        else if (routeNumber == 4)
+        {
+            route = new List<Transform>
+            {
+                wps[3],
+                wps[4],
+                wps[5],
+                wps[6]
+            };
+        }
+        else if (routeNumber == 5)
+        {
+            route = new List<Transform>
+            {
+                wps[3],
+                wps[4],
+                wps[5],
+                wps[7]
+            };
+        }
+        else if (routeNumber == 6)
+        {
+            route = new List<Transform>
+            {
+                wps[6],
+                wps[5],
+                wps[4],
+                wps[0]
+            };
+        }
+        else if (routeNumber == 7)
+        {
+            route = new List<Transform>
+            {
+                wps[6],
+                wps[5],
+                wps[4],
+                wps[3]
+            };
+        }
+        else if (routeNumber == 8)
+        {
+            route = new List<Transform>
+            {
+                wps[6],
+                wps[5],
+                wps[4],
+                wps[1],
+                wps[2]
+            };
+        }
+        else if (routeNumber == 9)
+        {
+            route = new List<Transform>
+            {
+                wps[7],
+                wps[5],
+                wps[4],
+                wps[0]
+            };
+        }
+        else if (routeNumber == 10)
+        {
+            route = new List<Transform>
+            {
+                wps[7],
+                wps[5],
+                wps[4],
+                wps[3]
+            };
+        }
+        else if (routeNumber == 11)
+        {
+            route = new List<Transform>
+            {
+                wps[7],
+                wps[5],
+                wps[4],
+                wps[1],
+                wps[2]
+            };
+        }
+        //initialise position and waypoint counter
+        transform.position = new Vector3(route[0].position.x, 0.0f, route[0].position.z);
+        targetWP = 1;
+    }
+ }
