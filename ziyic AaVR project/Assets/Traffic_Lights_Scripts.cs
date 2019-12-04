f1d3f9865c58eae679056b0713f0fb0a8b7a27a1
Cathal-Doherty <56649486+Cathal-Doherty@users.noreply.github.com>
2019-12-04 13:02:11 +0000

    1204

--- ziyic AaVR project/Assets/Traffic_Lights_Scripts.cs
+++ ziyic AaVR project/Assets/Traffic_Lights_Scripts.cs
@@ -5,23 +5,57 @@ using UnityEngine;
 public class Traffic_Lights_Scripts : MonoBehaviour
 {
     // Start is called before the first frame update
+    bool VAccess = true;
+
+    float timer;
+
+    GameObject TL1=new GameObject();
+    GameObject TL2;
+    GameObject TL3;
+
+    GameObject tl1g;
+    GameObject tl1r;
+    GameObject tl2g;
+    GameObject tl2r;
+    GameObject tl3g;
+    GameObject tl3r;
     void Start()
     {
-        var TL1 = GameObject.Find("TL1").transform;
-        var TL2 = GameObject.Find("TL2").transform;
-        var TL3 = GameObject.Find("TL3").transform;
-        var tl1g = TL1.FindChild("Green light");
-        var tl1r = TL1.FindChild("Red light");
-        var tl2g = TL1.FindChild("Green light");
-        var tl2r = TL1.FindChild("Red light");
-        var tl3g = TL1.FindChild("Green light");
-        var tl3r = TL1.FindChild("Red light");
+        TL1 = GameObject.Find("TL1");
+        TL1 = GameObject.Find("TL2");
+        TL1 = GameObject.Find("TL3");
+
+
+        tl1g = TL1.transform.Find("Green light").gameObject;
+        tl1r = TL1.transform.Find("Red light").gameObject;
+        tl2g = TL2.transform.Find("Green light").gameObject;
+        tl2r = TL2.transform.Find("Red light").gameObject;
+        tl3g = TL3.transform.Find("Green light").gameObject;
+        tl3r = TL3.transform.Find("Red light").gameObject;
+
+
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        timer = Time.deltaTime;
+        route(VAccess);
     }
+
+    void route (bool va)
+    {
+            tl1g.SetActive(!va);
+            tl1r.SetActive(va);
+
+            tl2g.SetActive(va);
+            tl2r.SetActive(!va);
+
+            tl3g.SetActive(va);
+            tl3r.SetActive(!va);
+    }
+
+
+
 }
